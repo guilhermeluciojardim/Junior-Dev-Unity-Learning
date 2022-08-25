bcd7a71ddde1769306eb3c627f59c7e4f53d3ae8
guilhermeluciojardim <guilhermelucio@gmail.com>
2022-08-25 13:15:19 -0300

    commit class tutorial
    - Add new Productivity unit
    - Created abstract, inheritance and polymorphism

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -20,14 +20,8 @@ public class UserControl : MonoBehaviour
         Marker.SetActive(false);
     }
 
-    private void Update()
-    {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        GameCamera.transform.position = GameCamera.transform.position + new Vector3(move.y, 0, -move.x) * PanSpeed * Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+    public void HandleSelection(){
+        var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
@@ -41,10 +35,9 @@ public class UserControl : MonoBehaviour
                 var uiInfo = hit.collider.GetComponentInParent<UIMainScene.IUIInfoContent>();
                 UIMainScene.Instance.SetNewInfoContent(uiInfo);
             }
-        }
-        else if (m_Selected != null && Input.GetMouseButtonDown(1))
-        {//right click give order to the unit
-            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+    }
+    public void HandleAction(){
+        var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
@@ -59,6 +52,19 @@ public class UserControl : MonoBehaviour
                     m_Selected.GoTo(hit.point);
                 }
             }
+    }
+    private void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        GameCamera.transform.position = GameCamera.transform.position + new Vector3(move.y, 0, -move.x) * PanSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleSelection();
+        }
+        else if (m_Selected != null && Input.GetMouseButtonDown(1))
+        {//right click give order to the unit
+            HandleAction();
         }
 
         MarkerHandling();
